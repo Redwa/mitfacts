15c93becf6dc4fc413c48497f195ad81d7923267
Redwa <radouane.elberrak@gmail.com>
2021-10-20 22:27:38 -0500

    Fixed Colors, Founded Range, and Motto

--- mit.js
+++ mit.js
@@ -1,7 +1,7 @@
 var mit = {
     city: 'Cambridge',
-    colors: ['pink', 'red'],
+    colors: ['Silver Gray'], // ['pink', 'red']
     mascot: "robot",
-    founded: 1900,
-    motto: 'Carpe diem',
+    founded: 1861, // was 1900
+    motto: 'Mens',
 };
